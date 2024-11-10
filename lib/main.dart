5c2d280215f5c25e34988644956a39dbbaf7f73d
mir666 <mirrabby633@gmail.com>
2024-11-10 20:41:22 +0600

    add

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Assignment',
       theme: ThemeData(
         appBarTheme: const AppBarTheme(
           color: Colors.amber,
